2f20ce753aedb4558a3f36301b89564c8c619075
LAPTOP-UMM7HUUJ\user <ntuananh2897@gmail.com>
2024-05-01 10:56:54 +0700

    create connection to db

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const webRouter = require('./routes/web');
 const configViewEngine = require('./config/viewEngine');
+const connection = require('./config/database');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -10,8 +11,21 @@ const app = express();
 configViewEngine(app)
 
 // Routes
-app.use('/', webRouter)
+app.use('/', webRouter);
 
-app.listen(port, () => {
-    console.log('Server run on: ' + port)
-})
+const startApp = () => {
+    app.listen(port, () => {
+        console.log('Server run on: ' + port)
+    })
+}
+
+// Connection
+;( async () => {
+    try {
+        await connection();
+        startApp()
+    } catch (error) {
+        console.log('Connection DB Error: ', error);
+    }
+})();
+
